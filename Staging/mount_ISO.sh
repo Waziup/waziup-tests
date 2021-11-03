bd4dbb39f7a506e6710fb6f76cd1869176c528bb
Corentin Dupont <corentin.dupont@gmail.com>
2021-11-03 13:57:16 +0100

    jenkins

--- Staging/mount_ISO.sh
+++ Staging/mount_ISO.sh
@@ -1,3 +1,6 @@
+# This script will unzip the ISO image in argument and mount it on /mnt/WaziGate_nightly.
+# It also modify the boot command to make it bootable from NFS.
+
 unzip -o deploy/image_$1.zip
 
 DEV=`losetup -f`
@@ -9,3 +12,4 @@ mount ${DEV}p1 /mnt/WaziGate_nightly/boot
 echo "console=serial0,115200 console=tty1 root=/dev/nfs nfsroot=172.16.11.191:/mnt/WaziGate_nightly,vers=3 rw ip=dhcp rootwait elevator=deadline" > /mnt/WaziGate_nightly/boot/cmdline.txt
 echo "" > /mnt/WaziGate_nightly/etc/fstab
 
+systemctl restart nfs-server
